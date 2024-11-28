f5808ed5b53429d49c682506507cd164d28b65e3
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-11-27 20:20:53 -0500

    Adding project

---
+++ msv-items/src/main/java/com/martin/cloud/items/msv_items/MsvItemsApplication.java
@@ -0,0 +1,13 @@
+package com.martin.cloud.items.msv_items;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsvItemsApplication.class, args);
+	}
+
+}
---
+++ msv-items/src/test/java/com/martin/cloud/items/msv_items/MsvItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.martin.cloud.items.msv_items;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
