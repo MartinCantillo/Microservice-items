a59a30215c325872fa875bb32069f453f4c80eb3
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-12-08 09:15:48 -0500

    updated: updating methos findbyid2

--- msv-items/src/main/java/com/martin/cloud/items/msv_items/controllers/ItemController.java
+++ msv-items/src/main/java/com/martin/cloud/items/msv_items/controllers/ItemController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.martin.cloud.items.msv_items.services.ItemService;
 
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,14 +29,14 @@ public class ItemController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Item> findById(@PathVariable Long id) {
+    public ResponseEntity<?> findById(@PathVariable Long id) {
 
-        Optional <Item> itemOptional=this.itemService.findById(id);
+        Optional<Item> itemOptional = this.itemService.findById(id);
         if (itemOptional.isPresent()) {
 
             return ResponseEntity.ok(itemOptional.get());
         }
-        return ResponseEntity.notFound().build();
+        return ResponseEntity.status(404).body(Collections.singletonMap("mesage", "Not found Product "));
     }
 
 }
