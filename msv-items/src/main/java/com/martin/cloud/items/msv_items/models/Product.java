74990e144b22a4fc5fd9cef9b7958abcedbfb7a3
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-12-07 20:31:46 -0500

    Adding lombok anotations

--- msv-items/src/main/java/com/martin/cloud/items/msv_items/models/Product.java
+++ msv-items/src/main/java/com/martin/cloud/items/msv_items/models/Product.java
@@ -1,5 +1,11 @@
 package com.martin.cloud.items.msv_items.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     private long id;
