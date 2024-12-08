9b8fa465f27d382f25cec81f1bc29ff9b056aea6
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-12-08 09:10:03 -0500

    updated: adding try catch

--- msv-items/src/main/java/com/martin/cloud/items/msv_items/services/ItemServiceFeign.java
+++ msv-items/src/main/java/com/martin/cloud/items/msv_items/services/ItemServiceFeign.java
@@ -11,6 +11,8 @@ import com.martin.cloud.items.msv_items.clients.ProductFeignClient;
 import com.martin.cloud.items.msv_items.models.Item;
 import com.martin.cloud.items.msv_items.models.Product;
 
+import feign.FeignException;
+
 @Service
 public class ItemServiceFeign implements ItemService{
 
@@ -36,11 +38,12 @@ public class ItemServiceFeign implements ItemService{
     @Override
     public Optional<Item> findById(Long id) {
 
-        Product product=this.client.finById(id);
-        if (product==null) {
+        try {
+            Product product=this.client.finById(id);
+            return Optional.ofNullable(new Item(product,new Random().nextInt(10)+1));
+        } catch (FeignException e) {
             return Optional.empty();
-        }
-       return Optional.ofNullable(new Item(product,new Random().nextInt(10)+1));
+        }      
     }
 
 }
