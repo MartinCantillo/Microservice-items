38a3dced369995e2495b3e9e00cf08054da0c822
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-12-07 21:11:28 -0500

    Update: updating ProductFeingClient

--- msv-items/src/main/java/com/martin/cloud/items/msv_items/clients/ProductFeignClient.java
+++ msv-items/src/main/java/com/martin/cloud/items/msv_items/clients/ProductFeignClient.java
@@ -1,8 +1,19 @@
 package com.martin.cloud.items.msv_items.clients;
 
-import org.springframework.cloud.openfeign.FeignClient;
+import java.util.*;
 
-@FeignClient
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.martin.cloud.items.msv_items.models.Product;
+
+@FeignClient(url="localhost:8081/api/product")
 public interface ProductFeignClient {
 
+    @GetMapping()
+    List<Product>findAll();
+
+    @GetMapping("/{id}")
+    Product finById(@PathVariable Long id);
 }
