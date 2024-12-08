676d7f0bf1057764876459bd444516bc6494e28f
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-12-08 08:25:28 -0500

    Added: itemService was added

---
+++ msv-items/src/main/java/com/martin/cloud/items/msv_items/services/ItemServiceFeign.java
@@ -0,0 +1,46 @@
+package com.martin.cloud.items.msv_items.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+
+import com.martin.cloud.items.msv_items.clients.ProductFeignClient;
+import com.martin.cloud.items.msv_items.models.Item;
+import com.martin.cloud.items.msv_items.models.Product;
+
+@Service
+public class ItemServiceFeign implements ItemService{
+
+    private final  ProductFeignClient client;
+
+    public ItemServiceFeign( ProductFeignClient clientFeign){
+        this.client = clientFeign;
+    }
+
+    
+
+    @Override
+    public List<Item> findAll() {
+       
+     return this.client.findAll().stream().map(
+        product->{
+           Random random = new Random();
+           return new Item(product,random.nextInt( 10)+1);
+        }
+     ).collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+
+        Product product=this.client.finById(id);
+        if (product==null) {
+            return Optional.empty();
+        }
+       return Optional.ofNullable(new Item(product,new Random().nextInt(10)+1));
+    }
+
+}
