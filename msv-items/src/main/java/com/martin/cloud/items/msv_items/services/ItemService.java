fa36af99db98aaa6f63e3e24b025c721a7a5129e
Martin Cantillo <martinjosecantillohernandez9@gmail.com>
2024-12-07 20:43:39 -0500

    Adding service ItemService

---
+++ msv-items/src/main/java/com/martin/cloud/items/msv_items/services/ItemService.java
@@ -0,0 +1,12 @@
+package com.martin.cloud.items.msv_items.services;
+
+import java.util.*;
+
+import com.martin.cloud.items.msv_items.models.Item;
+
+public interface ItemService {
+
+    List<Item>findAll();
+    
+    Optional<Item>findById();
+}
